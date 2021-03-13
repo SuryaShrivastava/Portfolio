47cac090abed280282d0a5beaf868ac334f59254
Vicky <suryashrivastava2001@gmail.com>
2021-03-13 20:54:49 +0530

    Portfolio

---
+++ javascript.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+    
+    let hamberger = document.querySelector('.hamberger');
+    let times = document.querySelector('.times');
+    let mobilenav = document.querySelector('.mobile-nav');
+    
+    hamberger.addEventListener('click', function(){
+        mobilenav.classList.add('open');
+    });
+    
+    times.addEventListener('click', function(){
+        mobilenav.classList.remove('open');
+    });
+    
+
+    
+});
